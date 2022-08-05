427d8297a306603275a97d1b64d87e6aa8c652b9
SfShev <pshev1406@gmail.com>
2022-08-06 00:21:09 +0300

    Написал метод заполнения главного массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿Console.Write("Введите длину массива: ");
+int arraysize = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Введите наибольшее количество символов строк,которые хотите увидеть: ");
+int numberofcharacters = Convert.ToInt32(Console.ReadLine());
+
+string[] mainarray = new string[arraysize];
+string[] resultarray = new string[arraysize];
+
+
+string[] Collection(int elements)
+{
+    Console.WriteLine("Заполните массив: ");
+    for (int i = 0; i < elements; i++)
+    {
+        Console.WriteLine();
+        Console.Write($"Введите элемент массива # {i + 1}: ");
+        mainarray[i] = Console.ReadLine()!;
+    }
+    return mainarray;
+}
